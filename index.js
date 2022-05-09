c2959c2b7e27af396f61121f8bb439a7424ab474
matheus.mazetti <matheus.mazetti.22@gmail.com>
2022-05-09 12:31:10 -0300

    chore: adjust code for deploy on heroku

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import cors from 'cors';
 import router from "./controllers/routes.js";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router)
-app.listen(5000);
+app.listen(process.env.PORT, () => {
+    console.log("Server running on port " + process.env.PORT);
+});
